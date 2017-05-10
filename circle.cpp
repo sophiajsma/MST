b83e307a4f4dc691a5190ef079b277f12ce95310
sophiajsma <494497541@qq.com>
2017-05-11 01:37:57 +0800

    p & k & cir right

--- circle.cpp
+++ circle.cpp
@@ -2,7 +2,7 @@
 #include<fstream>  
 using  namespace std;  
   
-#define MAX 100  
+#define MAX 200000+10  
 #define MAXCOST 0x7fffffff
 
 typedef struct{
@@ -27,7 +27,14 @@ int Wshall(int array[][MAX],int n){
     for(i = 1; i<=n ; ++i){
         for(j = 1; j <= n; ++j){
             r[i][j] = 0;
-            B[i][j] = array[i][j];
+            if(array[i][j] != 0)
+                B[i][j] = 1;
+            else
+                B[i][j] = 0;
+            // B[i][j] = array[i][j];
+            // if(B[i][j] < 0)
+                // cout <<array[i][j]<<endl;
+
         }
     }
    // cout << "it is in Wshall 2"<<endl;
@@ -36,7 +43,11 @@ int Wshall(int array[][MAX],int n){
         for(i = 1; i<=n ; ++i){
             if(B[i][j] >= 1){
                 for(k = 1; k <= n; k++){
-                    B[i][k] = B[i][k] + B[j][k];
+                    B[i][k] = B[i][k] + B[j][k] > 0 ? 1:0;
+                    // if(B[i][j] == 0)
+                        // B[i][j] = B[i][k] + B[k][j];
+                    // cout << i << ' '<<k<<' '<<B[i][k]<<endl;
+
                 }
             }
         }
@@ -52,7 +63,7 @@ int Wshall(int array[][MAX],int n){
         }
     }
    // // cout << "it is in Wshall 4"<<endl;
-
+    // cout << "m = "<< m << "n*n = "<<n*n<<endl;
     if(m == n*n) return 1;
     else return 0;
 }
@@ -63,36 +74,42 @@ int circle(int graph[][MAX], int n, int m)
     for(int i =0; i<m; ++i){
         p = a[i].x;
         q = a[i].y;
+        // cout << p << " "<<q <<  " "<< a[i].w << endl;
         // am = a[i].w;
         graph[p][q] = 0;
         graph[q][p] = 0;
         if(Wshall(graph,n) == 1){
             cout << "delete V" << p << "-> V" << q << "=" << a[i].w <<endl;
         }else{
+        // cout << "*+*"<<p << " "<<q <<  " "<< a[i].w << endl;
            graph[p][q] = a[i].w;
            graph[q][p] = a[i].w;
         }
     }
     int ans = 0;
+    int cnt = 0;
     for(int i = 1; i <= n; ++i){
-        for(int j = 1; j < i; ++j){
+        for(int j = 0; j < i; ++j){
             if(graph[i][j] != 0){
-                cout << "V" << i << "-> V" << j << "=" << graph[i][j] <<endl;
+                cnt +=1;
+                // cout << "V" << i << "-> V" << j << "=" << graph[i][j] <<endl;
                 ans+= graph[i][j];
             }
         }
     }
+    // cout <<"cnt is "<<cnt << endl;
     return ans;
 }  
   
 int main()  
 {  
-    int i, j, k, m, n;  
+    int i, j, k, m, n,aa;  
     int x, y, cost;  
     // ifstream in("input.txt");  
     // in >> m >> n;//n=顶点的个数，m=边的个数  
     //初始化图G  
-    cin >> n >> m;
+    ifstream in("100_70_2.txt");  
+    in >> n >> m >> aa;
     for (i = 1; i <= n; i++)  
     {  
         for (j = 1; j <= n; j++)  
@@ -111,13 +128,13 @@ int main()
     a[0].w = MAXCOST;
     for (k = 0; k < m; k++)  
     {  
-        // in >> i >> j >> cost;
-        cin >> i >> j >> cost;
-        a[k].x = i;
-        a[k].y = j;
+        in >> i >> j >> cost;
+        // cin >> i >> j >> cost;
+        a[k].x = i+1;
+        a[k].y = j+1;
         a[k].w = cost;  
-        graph[i][j] = cost;  
-        graph[j][i] = cost;  
+        graph[i+1][j+1] = cost;  
+        graph[j+1][i+1] = cost;  
     }  
     // for(int i=1;i<=n;++i){
     //     for(int j=1;j<=n;++j){
@@ -126,10 +143,10 @@ int main()
     //     cout <<"\n";
     // }
     qsort(a, m, sizeof(a[0]), cmp2);
-    cout << "*_*" << endl;
-    for(int i=0;i<m;++i){
-        cout << a[i].x <<' '<<a[i].y <<' '<<a[i].w <<endl;
-    }
+    // cout << "*_*" << endl;
+    // for(int i=0;i<m;++i){
+    //     cout << a[i].x <<' '<<a[i].y <<' '<<a[i].w <<endl;
+    // }
     //求解最小生成树  
     cost = circle(graph, n, m);  
     //输出最小权值和  
