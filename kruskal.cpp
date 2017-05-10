c774c4a9f0b4c0ed1cab6371f1cbcc3e2bcc7f59
sophiajsma <494497541@qq.com>
2017-05-10 23:45:44 +0800

    right k & p

--- kruskal.cpp
+++ kruskal.cpp
@@ -2,12 +2,13 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#define MAXN 10000 + 10
+#include <fstream>  
+#define MAXN 200000 + 10
 using namespace std;
 
 int par[MAXN], Rank[MAXN];
 typedef struct{
-    int x,y,w;
+    float x,y,w;
 }Node;
 Node a[MAXN];
 
@@ -62,33 +63,115 @@ int Kruskal(int n, int m){
     return res;
 }
 int main(){
-    int nPoint, mEdge, ans; // n 为 点数 m 为 边数
+    float nPoint, mEdge,aa, ans; // n 为 点数 m 为 边数
     // while(scanf("%d%d", &n, &m), n){
-        scanf("%d%d", &nPoint, &mEdge);
-
-        // cin >> nPoint >> mEdge;
+        // scanf("%d%d%d", &nPoint, &mEdge,&aa);
+        ifstream in("mst_1000_200000_0.txt");  
+        // in >> 
+        in >> nPoint >> mEdge >> aa;
+        cout  << nPoint << mEdge << aa <<endl;
         Init(nPoint);
         for(int i = 0; i < mEdge; i++){
-            scanf("%d%d%d", &a[i].x, &a[i].y, &a[i].w);
+            in >> a[i].x >> a[i].y >> a[i].w;
+            // scanf("%d%d%d", &a[i].x, &a[i].y, &a[i].w);
             //将村庄编号变为1~m（这个仅仅只是个人习惯，并非必要的）
             // a[i].x;
             // a[i].y;
         }
         ans = Kruskal(nPoint, mEdge);
         if(ans == -1) printf("?\n");
-        else printf("%d\n", ans);
+        else printf("%f\n", ans);
     // }
     return 0;
 }
 
-// 6 10  
-// 1 2 6  
-// 1 3 1  
-// 1 4 5  
-// 2 3 5  
-// 2 5 3  
-// 3 4 5  
-// 3 5 6  
-// 3 6 4  
-// 4 6 2  
-// 5 6 6
+// // 6 10  
+// // 1 2 6  
+// // 1 3 1  
+// // 1 4 5  
+// // 2 3 5  
+// // 2 5 3  
+// // 3 4 5  
+// // 3 5 6  
+// // 3 6 4  
+// // 4 6 2  
+// // 5 6 6
+// #include <cstdio>
+// #include <cstdlib>
+// #define MAXN 200000 + 10
+// using namespace std;
+
+// int par[MAXN], Rank[MAXN];
+// typedef struct{
+//     int a, b, price;
+// }Node;
+// Node a[MAXN];
+
+// int cmp(const void*a, const void *b){
+//     return ((Node*)a)->price - ((Node*)b)->price;
+// }
+// void Init(int n){
+//     for(int i = 0; i < n; i++){
+//         Rank[i] = 0;
+//         par[i] = i;
+//     }
+// }
+
+// int find(int x){
+//     int root = x;
+//     while(root != par[root]) root = par[root];
+//     while(x != root){
+//         int t = par[x];
+//         par[x] = root;
+//         x = t;
+//     }
+//     return root;
+// }
+
+// void unite(int x, int y){
+//     x = find(x);
+//     y = find(y);
+//     if(Rank[x] < Rank[y]){
+//         par[x] = y;
+//     }
+//     else{
+//         par[y] = x;
+//         if(Rank[x] == Rank[y]) Rank[x]++;
+//     }
+// }
+// //n为边的数量，m为村庄的数量
+// int Kruskal(int n, int m){
+//     int nEdge = 0, res = 0;
+//     //将边按照权值从小到大排序
+//     qsort(a, n, sizeof(a[0]), cmp);
+//     for(int i = 0; i < n && nEdge != m - 1; i++){
+//         //判断当前这条边的两个端点是否属于同一棵树
+//         if(find(a[i].a) != find(a[i].b)){
+//             unite(a[i].a, a[i].b);
+//             res += a[i].price;
+//             nEdge++;
+//         }
+//     }
+//     //如果加入边的数量小于m - 1,则表明该无向图不连通,等价于不存在最小生成树
+//     if(nEdge < m-1) res = -1;
+//     return res;
+// }
+// int main(){
+//     int n, m, ans,aa;
+//     // while(scanf("%d%d", &n, &m), n){
+//             ifstream in("mst_1000_200000_0.txt");  
+//         in >> m >> n >> aa;
+//         Init(m);
+//         for(int i = 0; i < n; i++){
+//             in >> a[i].a >> a[i].b >> a[i].price;
+//             // scanf("%d%d%d", &a[i].a, &a[i].b, &a[i].price);
+//             //将村庄编号变为0~m-1（这个仅仅只是个人习惯，并非必要的）
+//             // a[i].a--;
+//             // a[i].b--;
+//         }
+//         ans = Kruskal(n, m);
+//         if(ans == -1) printf("?\n");
+//         else printf("%d\n", ans);
+//     // }
+//     return 0;
+// }
--- prim.cpp
+++ prim.cpp
@@ -2,17 +2,18 @@
 #include<fstream>  
 using  namespace std;  
   
-#define MAX 100  
+#define MAX 200000 + 10  
 #define MAXCOST 0x7fffffff  
-  
+
 int graph[MAX][MAX];  
   
 int prim(int graph[][MAX], int n)  
 {  
     //n is the num of point
+    int cnt = 0;
     bool isU[MAX];
-    isU[1] = true;
-    for (int i = 2; i <= n;++i){
+    isU[0] = true;
+    for (int i = 1; i < n;++i){
         isU[i] = false;
     }
 
@@ -20,27 +21,32 @@ int prim(int graph[][MAX], int n)
     int mst[MAX];  
     int i, j, min, minid, sum = 0;  
 
-    for (int i = 2; i <= n; i++)  
+    for (int i = 1; i < n; i++)  
     {  
-        lowcost[i] = graph[1][i];  
-        mst[i] = 1;  
+        lowcost[i] = graph[0][i];  
+        mst[i] = 0;  
     }  
-    mst[1] = 0;  
-    for (int i = 2; i<=n; ++i){
+    mst[0] = -1; 
+    for (int i = 1; i<n; ++i){
         min = MAXCOST;
-        minid = 0;
-        for(j = 2; j <=n ;++j){
+        minid = 1;
+        for(j = 1; j <n ;++j){
             if(isU[j] == true)
                 continue;
+            // cout << lowcost[j]<<"&_&"<<endl;
             if(lowcost[j] < min){
+                cout << "*_*"<<endl;
                 min = lowcost[j];
                 minid = j;
             }
         }
-        cout << "V" << mst[minid] << "->" << minid << "=" << min << endl;
+        // cout << minid
+        cout << "V" << mst[minid] << "-> V" << minid << "=" << min << endl;
+        cnt += 1;
+        cout << "cnt is "<< cnt << endl;
         sum += min;
         isU[minid] = true;
-        for( int j = 2;j <=n ; ++j){
+        for(j = 1;j <n ; ++j){
             if(graph[minid][j] < lowcost[j]){
                 lowcost[j] = graph[minid][j];
                 mst[j] = minid;
@@ -48,21 +54,33 @@ int prim(int graph[][MAX], int n)
         }
     }
 
-    
+    for(int i=0;i<n;++i){
+        if(isU[i] == false){
+            cout  << "i is "<< i << endl;
+
+        }
+    }
     return sum;  
 }  
   
 int main()  
 {  
-    int i, j, k, m, n;  
+    // int **graph=new int*[MAX];       //分配一个指针数组，将其首地址保存在b中                                                    
+    //        for(int i=0;i<MAX;i++)             //为指针数组的每个元素分配一个数组
+    //            graph[i]=new int[MAX];  
+    int i, j, k, m, n,aa;  
     int x, y, cost;  
     // ifstream in("input.txt");  
     // in >> m >> n;//n=顶点的个数，m=边的个数  
-    //初始化图G  
-    cin >> n >> m;
-    for (i = 1; i <= n; i++)  
+    //初始化图G 
+     ifstream in("mst_1000_200000_0.txt");  
+        // in >> 
+    in >> n >> m >> aa; 
+    // cout << n << m << aa;
+    // cin >> n >> m;
+    for (i = 0; i <n; i++)  
     {  
-        for (j = 1; j <= n; j++)  
+        for (j = 0; j < n; j++)  
         {  
             graph[i][j] = MAXCOST;  
         }  
@@ -70,11 +88,20 @@ int main()
     //构建图G  
     for (k = 1; k <= m; k++)  
     {  
-        // in >> i >> j >> cost;
-        cin >> i >> j >> cost;  
+        in >> i >> j >> cost;
+        // cin >> i >> j >> cost; 
+        // cout <<"cost is "<<cost<<endl; 
         graph[i][j] = cost;  
-        graph[j][i] = cost;  
-    }  
+        graph[j][i] = cost; 
+        // cout << graph[i][j]<<endl; 
+    }
+    // for(i = 1;i<=100;++i){
+    //     for (k=1;k<=100;++k){
+    //         if(graph[i][k] != MAXCOST)
+    //             cout <<graph[i][k]<<' ';
+    //     }
+    //     // cout<<endl;
+    // }  
     //求解最小生成树  
     cost = prim(graph, n);  
     //输出最小权值和  
